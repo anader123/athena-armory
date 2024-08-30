949c9994cbf569b87efa73120c5e7283a20c4aac
Nader <alannaderdev@gmail.com>
2024-08-30 10:25:31 -0600

    add website dir

---
+++ website/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h2 className="text-2xl">Athena's Armory</h2>
+        <h2 className="font-open-sans text-gray-400">
+          testadfgasedtrfsdfasdf asd
+        </h2>
+      </div>{" "}
+    </main>
+  );
+}
