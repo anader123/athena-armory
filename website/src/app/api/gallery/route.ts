97257088117135b30610b6547362934eae529eb6
Nader <alannaderdev@gmail.com>
2024-09-10 12:12:05 -0600

    resolve gallery network id

--- website/src/app/api/gallery/route.ts
+++ website/src/app/api/gallery/route.ts
@@ -1,9 +1,14 @@
 import { NextResponse } from "next/server";
 import { DEPLOYMENTS } from "@/constants/addresses";
+import { base } from "viem/chains";
 
 export async function GET(): Promise<NextResponse> {
   const networkId = +(process.env.NEXT_PUBLIC_NETWORK_ID as string);
-  const url = `https://api-base-sepolia.reservoir.tools/tokens/v7?collection=${DEPLOYMENTS[networkId].zoraContract}&sortBy=tokenId`;
+  const url = `https://api-base${
+    networkId === base.id ? "" : "-sepolia"
+  }.reservoir.tools/tokens/v7?collection=${
+    DEPLOYMENTS[networkId].zoraContract
+  }&sortBy=tokenId`;
 
   try {
     const response = await fetch(url, {
